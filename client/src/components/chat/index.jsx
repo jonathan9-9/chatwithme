2a9a69371c81e6fa0aa8aec896e58ec3098fd69b
Jonathan <jonathancornejox@gmail.com>
2023-10-14 15:43:52 -0700

    added custom header render function and pass props to header function

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import {
+  useMultiChatLogic,
+  MultiChatSocket,
+  MultiChatWindow,
+} from "react-chat-engine-advanced";
+import Header from "@/components/customHeader";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const chatProps = useMultiChatLogic(
+    // to retrieve environment variable from env.local
+    import.meta.env.VITE_PROJECT_ID,
+    "testuser",
+    "1999"
+  );
+  return (
+    <div style={{ flexBasis: "100%" }}>
+      {/* SOCKET */}
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
+        {...chatProps}
+        style={{
+          height: "100vh",
+        }}
+        renderChatHeader={(chat) => <Header chat={chat} />}
+      />
+    </div>
+  );
 };
 
 export default Chat;
