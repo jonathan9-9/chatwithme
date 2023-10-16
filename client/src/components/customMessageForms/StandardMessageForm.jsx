42048db90f7fbb402e7573e57df6c9dc60d9d0e8
Jonathan <jonathancornejox@gmail.com>
2023-10-15 17:51:14 -0700

    react dropzone component

--- client/src/components/customMessageForms/StandardMessageForm.jsx
+++ client/src/components/customMessageForms/StandardMessageForm.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
+import { PaperClipIcon, XMarkIcon } from "@heroicons/react/24/solid";
+import DropZone from "react-dropzone";
 
 const StandardMessageForm = () => {
   const [message, setMessage] = useState("");
   const [attachment, setAttachment] = useState("");
   const [preview, setPreview] = useState("");
+
+  const handleChange = (e) => setMessage(e.target.value);
 
   return (
     <div className="message-form-container">
@@ -15,8 +19,47 @@ const StandardMessageForm = () => {
             src={preview}
             onLoad={() => URL.revokeObjectURL(preview)}
           />
+          <XMarkIcon
+            className="message-form-icon-x"
+            onClick={() => {
+              setPreview("");
+              setAttachment("");
+            }}
+          />
         </div>
       )}
+      <div className="message-form">
+        <div className="message-form-input-container">
+          <input
+            className="message-form-input"
+            type="text"
+            value={message}
+            onChange={handleChange}
+            placeholder="Type a message..."
+          />
+        </div>
+        <div className="message-form-icons">
+          <DropZone
+            acceptedFiles=".jpg,.jpeg,.png"
+            multiple={false}
+            noClick={true}
+            onDrop={(acceptedFiles) => {
+              setAttachment(acceptedFiles[0]);
+              setPreview(URL.createObjectURL(acceptedFiles[0]));
+            }}
+          >
+            {({ getRootProps, getInputProps, open }) => (
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <PaperClipIcon
+                  className="message-form-icon-clip"
+                  onClick={open}
+                />
+              </div>
+            )}
+          </DropZone>
+        </div>
+      </div>
     </div>
   );
 };
