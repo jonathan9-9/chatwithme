58f7714e01379ef8e848e4728538e931b31adf9c
Jonathan <jonathancornejox@gmail.com>
2023-10-19 13:10:28 -0700

    api call to frontend via axios and openai api

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -10,7 +10,27 @@ router.post("/text", async (req, res) => {
   try {
     const { text, activeChatId } = req.body;
 
-    res.status(200).json({ text });
+    const response = await openai.chat.completions.create({
+      model: "text-davinci-003",
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        { role: "user", content: text },
+      ],
+    });
+
+    await axios.post(
+      `https://api.chatengine.io/chats/${activeChatId}/messages/`,
+      { text: response.data.choices[0].message.content },
+      {
+        headers: {
+          "Project-ID": process.env.PROJECT_ID,
+          "User-Name": process.env.BOT_USER_NAME,
+          "User-Secret": process.env.BOT_USER_SECRET,
+        },
+      }
+    );
+
+    res.status(200).json({ text: response.data.choices[0].message.content });
   } catch (error) {
     console.error("error", error);
     res.status(500).json({ error: error.message });
